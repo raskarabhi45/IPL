303c67f685412bae610d0c1dcefabf7bc84c4814
raskarabhi45 <raskarabhi45@gmail.com>
2023-10-12 17:04:54 +0530

    Comleted UI element for TeamPage

---
+++ demo/src/frontend/src/pages/MatchPage.jsx
@@ -0,0 +1,15 @@
+import {React,useEffect,useState} from 'react'
+import MatchDetailedCard from '../components/MatchDetailedCard';
+import MatchSmallCard from '../components/MatchSmallCard';
+import { useParams } from 'react-router-dom';
+
+const MatchPage = () => {
+
+  return (
+  <>
+    <h1>Match Page</h1>
+  </>
+  )
+}
+
+export default MatchPage;
